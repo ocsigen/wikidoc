6274f0892bfbeaa79e931427ec8508bfa37b6ee8
Grégoire Henry <gregoire.henry@ocamlpro.com>
2014-07-14 18:26:14 +0200

    Jenkins: build_doc.

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,5 +1,13 @@
+
 opam pin add --no-action wikidoc .
 opam install --deps-only wikidoc
 opam install --verbose wikidoc
-opam remove --verbose wikidoc
+
+do_build_doc () {
+  # Nothing...
+}
+
+do_remove () {
+  opam remove --verbose wikidoc
+}
 
